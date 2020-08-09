70c9830ea597cb527898e689b12d3d22602a60ef
Ilya <szrnamerg@gmail.com>
2020-08-09 23:25:09 +0400

    visual upgrade of Codex Marianus Parsing

--- CodexMarianusParsing.Designer.cs
+++ CodexMarianusParsing.Designer.cs
@@ -34,15 +34,17 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(401, 12);
+            this.button1.Location = new System.Drawing.Point(365, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(221, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Загрузить файл";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // openFileDialog1
@@ -66,27 +68,41 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(629, 157);
+            this.button2.Location = new System.Drawing.Point(555, 157);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(216, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Поместить в БД";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(629, 248);
+            this.button3.Location = new System.Drawing.Point(555, 248);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(216, 23);
             this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
+            this.button3.Text = "Провести частеречную разметку";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(249, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Полученные данные";
             // 
             // CodexMarianusParsing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
@@ -95,6 +111,7 @@
             this.Name = "CodexMarianusParsing";
             this.Text = "CodexMarianusParsing";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -106,5 +123,7 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label label1;
     }
 }
